dc1cf044a612bef4186adb1d694c96aecfb42f69
hotteok219 <helen-thai@hotmail.com>
2023-03-22 22:56:41 +1100

    First commit of draft working pages

---
+++ script/main.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById("page-nav-open").style.visibility = "visible";
+    document.getElementById("page-nav-open").style.width = "50vw"
+}
+
+function closeMenu() {
+    document.getElementById("page-nav-open").style.visibility = "collapse";
+    document.getElementById("page-nav-open").style.width = "0vw"
+}
